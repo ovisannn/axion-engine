a516e3d645b91581c7f1083082dcf53ada4995a7
DESKTOP-KM56JIU\ovisa <ovi.sanjaya11@gmail.com>
2026-05-27 19:47:14 +0700

    add mouse event

---
+++ Axion/src/Axion/Event/MouseEvent.h
@@ -0,0 +1,26 @@
+namespace Axion {
+	class MouseMovedEvent : public Event {
+	public:
+		MouseMovedEvent(float x, float y)
+			: m_MouseX(x), m_MouseY(y) {
+		}
+		inline float GetX() const { return m_MouseX; }
+		inline float GetY() const { return m_MouseY; }
+		EventType GetEventType() const override { return EventType::MouseMoved; }
+		const char* GetName() const override { return "MouseMovedEvent"; }
+	private:
+		float m_MouseX, m_MouseY;
+	};
+	class MouseScrolledEvent : public Event {
+	public:
+		MouseScrolledEvent(float xOffset, float yOffset)
+			: m_XOffset(xOffset), m_YOffset(yOffset) {
+		}
+		inline float GetXOffset() const { return m_XOffset; }
+		inline float GetYOffset() const { return m_YOffset; }
+		EventType GetEventType() const override { return EventType::MouseScrolled; }
+		const char* GetName() const override { return "MouseScrolledEvent"; }
+	private:
+		float m_XOffset, m_YOffset;
+	};
+}
